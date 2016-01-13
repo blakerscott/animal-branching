39f34d1018b2e74371042f9aa0b05f43082d1877
Blake Scott and Christine Tacha <tacha.christine@gmail.com>
2016-01-13 11:06:37 -0800

    add images to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
 
   var turtleClick = confirm("Do you want to learn about Turtles?");
+  var snakeClick = confirm("Do you want to learn about Snakes?");
+  var frogClick = confirm("Do you want to learn about frogs?");
 
   if (turtleClick === true) {
     $("#turtles").show();
   } else {
     $(".turtles").hide();
+  }
+
+  if (snakeClick === true) {
+    $("#snakes").show();
+  } else {
+    $(".snakes").hide();
+  }
+
+  if (frogClick === true) {
+    $("#frogs").show();
+  } else {
+    $(".frogs").hide();
   }
 
 
